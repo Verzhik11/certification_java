c41e18f6a3623458bf9a10796339efb1b3303cf6
verzhik <verzhik11@gmail.com>
2023-12-25 18:07:27 +0530

    Tests start

---
+++ src/test/java/service/FilterFlightsTest.java
@@ -0,0 +1,39 @@
+package service;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import static org.assertj.core.api.Assertions.assertThat;
+
+class FilterFlightsTest {
+
+    @BeforeEach
+    void setUp() {
+
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    void pastFlights() {
+    }
+
+    @Test
+    void timeZone() {
+    }
+
+    @Test
+    void longTransfer() {
+    }
+
+    @Test
+    void thisDay() {
+    }
+
+    @Test
+    void kindOfFlight() {
+    }
+}
